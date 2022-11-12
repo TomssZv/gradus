b681d7463d89849ce406e73ca6c3d15788593d46
jacob <tomsszvirbulis@gmail.com>
2022-11-12 16:13:57 +0200

    register data sent to server

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,22 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 import '../style/RegisterLogin.css'
 
 function Register() {
+  const [user, setUser] = useState(
+    {
+      name: null,
+      surname: null,
+      username: null,
+      email: null,
+      password: null,
+      confirmPassword: null
+    }
+  )
+
+  const handleSubmit = (e) => {
+    console.log(user)
+    e.preventDefault()
+    axios.post(`http://localhost:5000/register`,
+     { user },
+     {
+     headers: {
+      "Content-type": "application/json; charset=UTF-8",
+      }})
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
   return (
     <div className='register form-cont'>
-        <form id='register-form' className='form'>
+        <form onSubmit={handleSubmit} id='register-form' className='form'>
             <label>First name</label>
-            <input name='first-name' type='text' placeholder='Ex: Joe' required></input>
+            <input onChange={(e) => {user.name = e.target.value}} name='first-name' type='text' placeholder='Ex: Joe' required></input>
             <label>Second name</label>
-            <input name='first-name' type='text' placeholder='Ex: Joe' required></input>
+            <input onChange={(e) => {user.surname = e.target.value}} name='first-name' type='text' placeholder='Ex: Joe' required></input>
             <label>Username</label>
-            <input name='first-name' type='text' placeholder='Ex: Joe' required></input>
+            <input onChange={(e) => {user.username = e.target.value}} name='first-name' type='text' placeholder='Ex: Joe' required></input>
             <label>E-mail</label>
-            <input name='first-name' type='email' placeholder='Ex: Joe' required></input>
+            <input onChange={(e) => {user.email = e.target.value}} name='first-name' type='email' placeholder='Ex: Joe' required></input>
             <label>Password</label>
-            <input name='first-name' type='password' placeholder='password' required></input>
+            <input onChange={(e) => {user.password = e.target.value}} name='first-name' type='password' placeholder='password' required></input>
             <label>Confirm password</label>
-            <input name='first-name' type='password' placeholder='password' required></input>
+            <input onChange={(e) => {user.confirmPassword = e.target.value}} name='first-name' type='password' placeholder='password' required></input>
             <button type='submit'>Register</button>
         </form>
     </div>
--- server/index.js
+++ server/index.js
@@ -1,24 +1,34 @@
 const express = require('express');
 const mysql = require('mysql')
+const cors = require('cors')
 const dotenv = require('dotenv') // .env file
 dotenv.config()
+const app = express();
 
+app.use(express.urlencoded({extended:true}));
+app.use(cors())
 
 const db = mysql.createConnection({
     host: process.env.HOST,
-    user: process.env.USER,
+    user: process.env.DB_USER,
     password: process.env.PASSWORD,
     database: process.env.DB
 })
 
 
-const app = express();
-
 app.get('/', (req, res) => {
     db.query("select * from users", (error, result, field) => {
-        if (error) throw error
+        if (error) {
+            console.log(error)
+        }
         res.json(result)
     })
+})
+
+app.post('/register', (req, res) => {
+    res.send('works')
+    console.log(req.body)
+    res.end()
 })
 
 app.listen(5000, () => {
